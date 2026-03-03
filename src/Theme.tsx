706d22c5c4b9f5f22e4b80ab5c41aa12e918196c
Matt Stone <mstone121@gmail.com>
2026-03-02 18:34:53 -0600

    Enable dark mode

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -2,9 +2,19 @@ import type { ReactNode } from "react";
 import { createTheme, ThemeProvider } from "@mui/material";
 
 const theme = createTheme({
-	palette: {
-		primary: { main: "#000000" },
-		secondary: { main: "#ffffff" },
+	colorSchemes: {
+		dark: {
+			palette: {
+				primary: { main: "#ffffff" },
+				secondary: { main: "#000000" },
+			},
+		},
+		light: {
+			palette: {
+				primary: { main: "#000000" },
+				secondary: { main: "#ffffff" },
+			},
+		},
 	},
 	typography: {
 		fontFamily: "sans-serif",
